f564365369c0f84f41c32093cdb5bc62fc95db64
ElCurdente <jules.mesnil@gmail.com>
2022-10-05 14:40:19 +0200

    login spotify

--- front/src/App.js
+++ front/src/App.js
@@ -14,11 +14,38 @@ function App() {
   const [users, setUsers] = useState([]);
   const [socket, setSocket] = useState(null);
 
+  const CLIENT_ID = "267dee8acb604afd81a5b1464a34b77a"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+
   useEffect(() => {
     const newSocket = io("https://whispering-chamber-09886.herokuapp.com/");
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
 
   useEffect(() => {
     if (socket) {
@@ -78,6 +105,10 @@ function App() {
     <div className="App">
       <InputName socket={socket} />
       <InputMessage socket={socket} />
+      {!token ?
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+          to Spotify</a>
+        : <button onClick={logout}>Logout</button>}
       {socket ? (
         <>
           <ThreadMessages messages={messages} />
